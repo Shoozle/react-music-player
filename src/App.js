02cbc6ed705c79f93d64948008e86b01d4e10f7e
Shoozle <sean@seanmurtagh.uk>
2021-10-29 09:58:23 +0100

    Display song data for index0

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 import data from './util';
+import { useState } from 'react';
 
 function App() {
+  //Array returned from data set to state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song/>
+      <Song currentSong={currentSong}/>
       <Player/>
     </div>
   );

117593a43c111303344c51ea8343bafd165b5341
Shoozle <sean@seanmurtagh.uk>
2021-10-31 06:34:33 +0000

    Removed useeffect

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,24 +4,6 @@ import { useEffect } from "react";
 
 const Player = ({ audioRef, songInfo, setSongInfo, songs, setSongs, currentSong, isPlaying, setCurrentSong, setIsPlaying }) => {
 
-    useEffect(() => {
-        const newSongs = songs.map((sng) => {
-            if (sng.id === currentSong.id) {
-                return {
-                    ...sng,
-                    active: true
-                }
-            } else {
-                return {
-                    ...sng,
-                    active: false
-                }
-            }
-        })
-
-        setSongs(newSongs);
-    }, [currentSong])
-
     const playSongHandler = (e) => {
         const audio = audioRef.current;
         if (isPlaying) {
@@ -49,18 +31,39 @@ const Player = ({ audioRef, songInfo, setSongInfo, songs, setSongs, currentSong,
         })
     }
 
+    const activeLibraryHandler = (nextPrev) => {
+        const newSongs = songs.map((sng) => {
+            if (sng.id === nextPrev.id) {
+                return {
+                    ...sng,
+                    active: true
+                }
+            } else {
+                return {
+                    ...sng,
+                    active: false
+                }
+            }
+        })
+
+        setSongs(newSongs);
+    }
+
     const skipTrackHandler = async (dir) => {
         let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
         if (dir === 'skipforward') {
-            await setCurrentSong(songs[(currentIndex + 1) % songs.length])
+            await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+            activeLibraryHandler(songs[(currentIndex + 1) % songs.length])
         }
         if (dir === 'skipback') {
             if ((currentIndex - 1) === -1) {
                 await setCurrentSong(songs[songs.length - 1])
+                activeLibraryHandler(songs[songs.length - 1])
                 if (isPlaying) audioRef.current.play();
                 return;
             }
-            await setCurrentSong(songs[(currentIndex - 1) % songs.length])
+            await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+            activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
         }
         if (isPlaying) audioRef.current.play();
     }
